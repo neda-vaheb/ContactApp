d0312cc058846a6811fe58c39cab29a98bc92b69
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-08-18 06:11:21 -0700

    function checkFiled is add to js file to check the input that they arent empty and showError function  to display the error

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,7 +1,24 @@
 const form = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
 const button = document.querySelector("button");
+const errorContainer = document.querySelectorAll("span");
 
+const showError = (text, index) => {
+    errorContainer[index].innerText = text;
+    errorContainer[index].style.display = "inline-block";
+  };
+  
+  const checkField = () => {
+      inputs.forEach((input, index) => {
+        input.classList.remove("inputError");
+        if (!input.value) {
+          showError("A required field has been missed", index);
+          input.className = "inputError";
+        } else {
+          errorContainer[index].innerText = "";
+        }
+      });
+    }; 
 const buttonHandler = (event) => {
     event.preventDefault();
    
