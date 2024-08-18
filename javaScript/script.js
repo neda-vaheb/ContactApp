300e5ad77394b5278886bda2ff94770f53db385b
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-08-18 07:11:20 -0700

    Add media to the css file to make the form responsive

--- javaScript/script.js
+++ javaScript/script.js
@@ -7,102 +7,98 @@ const queryItems = document.getElementsByName("query");
 const message = document.getElementById("message");
 const checkBox = document.getElementById("checkbox");
 const successMassage = document.getElementById("successMassage");
- let isAccept = false; 
+let isAccept = false;
 
 const showError = (text, index) => {
-    errorContainer[index].innerText = text;
-    errorContainer[index].style.display = "inline-block";
-  };
-  
-  const checkField = () => {
-      inputs.forEach((input, index) => {
-        input.classList.remove("inputError");
-        if (!input.value) {
-          showError("A required field has been missed", index);
-          input.className = "inputError";
-        } else {
-          errorContainer[index].innerText = "";
-        }
-      });
-    }; 
-
-    const emailValidation = () => {
-        const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-        if (!emailPattern.test(email.value)) {
-          showError("Please enter the valid email", 2);
-        }
-      };
-
-      const checkQueryField = () => {
-        let querySelected = false;
-      
-        queryItems.forEach((item) => {
-          item.parentElement.classList.remove("query-item-check");
-          if (item.checked) {
-            querySelected = true;
-            item.parentElement.classList.add("query-item-check");
-          }
-          return querySelected;
-        });
-        querySelected
-          ? (errorContainer[3].innerText = "")
-          : showError("A required field has been missed", 3);
-      };
-
-      const messageStatus = () => {
-        const messageValue = message.value.trim();
-        if (!messageValue) {
-          showError("A required field has been missed", 4);
-          message.className = "inputError";
-        } else {
-          message.classList.remove("inputError");
-          errorContainer[4].innerText = "";
-        }
-      };
-      const checkBoxStatus = () => {
-        const isCheck = checkBox.checked;
-        isCheck
-          ? (errorContainer[5].innerText = "")
-          : showError("To submit this form,Please consent to beaning contacted", 5);
-      };
-
-      const toShowSuccessMessage = ()=>{
-        errorContainer.forEach((error) => {
-            if(error.innerText) {
-                isAccept= false;
-                return isAccept;
-            }else{
-                return isAccept=true;
-            }
-                
-          });
-          if(isAccept){
-            
-            successMassage.style.display = "block";
-            setTimeout(()=>{
-                successMassage.style.display = "none";
-                inputs.forEach(input=>{
-                    input.value ="";
-                    message.value ="";
-                    form.reset();
-                })
-            },3000)
-        }
-      }
-      
-
-const buttonHandler = (event) => {
-    event.preventDefault();
-    checkField();
-    emailValidation();
-    checkQueryField();
-    messageStatus();
-    checkBoxStatus();
-    toShowSuccessMessage();
+  errorContainer[index].innerText = text;
+  errorContainer[index].style.display = "inline-block";
 };
 
+const checkField = () => {
+  inputs.forEach((input, index) => {
+    input.classList.remove("inputError");
+    if (!input.value) {
+      showError("A required field has been missed", index);
+      input.className = "inputError";
+    } else {
+      errorContainer[index].innerText = "";
+    }
+  });
+};
+
+const emailValidation = () => {
+  const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!emailPattern.test(email.value)) {
+    showError("Please enter the valid email", 2);
+  }
+};
+
+const checkQueryField = () => {
+  let querySelected = false;
+
+  queryItems.forEach((item) => {
+    item.parentElement.classList.remove("query-item-check");
+    if (item.checked) {
+      querySelected = true;
+      item.parentElement.classList.add("query-item-check");
+    }
+    return querySelected;
+  });
+  querySelected
+    ? (errorContainer[3].innerText = "")
+    : showError("A required field has been missed", 3);
+};
+
+const messageStatus = () => {
+  const messageValue = message.value.trim();
+  if (!messageValue) {
+    showError("A required field has been missed", 4);
+    message.className = "inputError";
+  } else {
+    message.classList.remove("inputError");
+    errorContainer[4].innerText = "";
+  }
+};
+const checkBoxStatus = () => {
+  const isCheck = checkBox.checked;
+  isCheck
+    ? (errorContainer[5].innerText = "")
+    : showError("To submit this form,Please consent to beaning contacted", 5);
+};
+
+const toShowSuccessMessage = () => {
+  errorContainer.forEach((error) => {
+    if (error.innerText) {
+      isAccept = false;
+      return isAccept;
+    } else {
+      return (isAccept = true);
+    }
+  });
+  if (isAccept) {
+    successMassage.style.display = "block";
+    setTimeout(() => {
+      successMassage.style.display = "none";
+      inputs.forEach((input) => {
+        input.value = "";
+        message.value = "";
+        form.reset();
+      });
+    }, 3000);
+  }
+};
+
+const buttonHandler = (event) => {
+  event.preventDefault();
+  checkField();
+  emailValidation();
+  checkQueryField();
+  messageStatus();
+  checkBoxStatus();
+  toShowSuccessMessage();
+};
 
 button.addEventListener("click", buttonHandler);
-form.addEventListener("mousedown",(event)=>{
-    event.preventDefault();
-})
+form.addEventListener("mousedown", (event) => {
+  event.preventDefault();
+});
