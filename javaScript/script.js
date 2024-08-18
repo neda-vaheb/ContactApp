e3e27d78f1b36d3192db99b5555083f4f9b9b48d
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-08-18 06:06:54 -0700

    start working in javaScript file, get inputs ,form and button

---
+++ javaScript/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+const button = document.querySelector("button");
+
+const buttonHandler = (event) => {
+    event.preventDefault();
+   
+};
+
+button.addEventListener("click", buttonHandler);
+
